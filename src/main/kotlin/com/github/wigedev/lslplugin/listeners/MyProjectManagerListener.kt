c729205bf604434979d4c2df43996ee1e335196f
GitHub Action <action@github.com>
2022-12-22 15:01:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/wigedev/lslplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/wigedev/lslplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.wigedev.lslplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.wigedev.lslplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/wigedev/lslplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wigedev/lslplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wigedev.lslplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wigedev.lslplugin.MyBundle
 
 class MyApplicationService {
 
